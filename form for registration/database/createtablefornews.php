5df7a0978f8fa97cac0c767033511e3b4b8e02ed
Vod1chka <99795108+Vod1chka@users.noreply.github.com>
2022-03-24 15:19:40 +0800

    Update createtablefornews.php

--- form for registration/database/createtablefornews.php
+++ form for registration/database/createtablefornews.php
@@ -11,9 +11,9 @@ try {
 	ntext text collate utf8_unicode_ci NOT NULL,
 	newstext text collate utf8_unicode_ci NOT NULL,
 	ntitle varchar(255) collate utf8_unicode_ci NOT NULL,
-    ndate varchar(255) collate utf8_unicode_ci NOT NULL,
-    nuser varchar(255) collate utf8_unicode_ci NOT NULL
-    )";
+    	ndate varchar(255) collate utf8_unicode_ci NOT NULL,
+    	nuser varchar(255) collate utf8_unicode_ci NOT NULL
+   	)";
     $conn->exec($sql);
 
     /* Вывод сообщения об успехе */
